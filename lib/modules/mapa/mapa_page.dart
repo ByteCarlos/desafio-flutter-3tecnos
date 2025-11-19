1401e48e0e37b5669a93ae3279bf66f74ef0b9d3
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2025-11-19 20:00:56 -0300

    feat: create mapa page

---
+++ lib/modules/mapa/mapa_page.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapaPage extends StatefulWidget {
+  const MapaPage({super.key});
+
+  @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  late GoogleMapController mapController;
+
+  LatLng posicaoAtual = const LatLng(-15.7797, -47.9297); // Brasília default
+
+  final latController = TextEditingController();
+  final lngController = TextEditingController();
+
+  Future pegarLocalizacao() async {
+    final pos = await Geolocator.getCurrentPosition();
+    setState(() {
+      posicaoAtual = LatLng(pos.latitude, pos.longitude);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pegarLocalizacao();
+  }
+
+  void atualizarLocal() {
+    final lat = double.tryParse(latController.text);
+    final lng = double.tryParse(lngController.text);
+
+    if (lat != null && lng != null) {
+      setState(() {
+        posicaoAtual = LatLng(lat, lng);
+      });
+      mapController.animateCamera(
+        CameraUpdate.newLatLng(posicaoAtual),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Mapa")),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              onMapCreated: (c) => mapController = c,
+              initialCameraPosition: CameraPosition(
+                target: posicaoAtual,
+                zoom: 14,
+              ),
+              markers: {
+                Marker(markerId: const MarkerId("Atual"), position: posicaoAtual)
+              },
+            ),
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(12),
+            color: Colors.grey.shade200,
+            child: Column(
+              children: [
+                TextField(
+                  controller: latController,
+                  decoration: const InputDecoration(labelText: "Latitude"),
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  controller: lngController,
+                  decoration: const InputDecoration(labelText: "Longitude"),
+                  keyboardType: TextInputType.number,
+                ),
+                ElevatedButton(
+                  onPressed: atualizarLocal,
+                  child: const Text("Exibir localização"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
