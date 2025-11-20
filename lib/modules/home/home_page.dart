7648eb330c228e3bdb35325273380e129816ccc1
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2025-11-20 11:02:21 -0300

    fix: ajustes visuais na home

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,30 +1,98 @@
+import 'package:desafio_3tecnos/shared/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import '../../core/routes/app_routes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.black.withOpacity(0.7),
-              foregroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.15,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo_3tecnos.png"),
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
-            onPressed: () {
-              Navigator.pushNamed(context, AppRoutes.mapa);
-            },
-            child: const Text("Abrir Mapa"),
           ),
+
+          // Conteúdo principal
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Bem vindo",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.grey,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                Text(
+                  "Esse é meu teste",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: AppColors.secondary,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(22)),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() => currentIndex = index);
+
+            if (index == 1) {
+              Navigator.pushNamed(context, AppRoutes.mapa);
+            }
+          },
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white70,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 28),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map, size: 28),
+              label: "Mapa",
+            ),
+          ],
         ),
       ),
     );
